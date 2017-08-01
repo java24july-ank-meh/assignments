01dbb0d747e2c81b53865732df2c6c486318a191
Chris Palmour <chpalmour@gmail>
2017-08-01 09:30:04 -0400

    QC practice (7-29)

---
+++ 7-29/Casting.java
@@ -0,0 +1,13 @@
+public class Casting{
+    public static void main(String[] args){
+        /*
+        //int i = 2.3; //won't compile
+        int i2 = (int) 2.3;
+        double d = 5;
+        //System.out.println(i);
+        System.out.println(i2);
+        System.out.println(5);
+        */
+
+    }
+}
