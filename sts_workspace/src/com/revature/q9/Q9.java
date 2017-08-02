bedec01ac2fc9b6f208490fdd643e56bf117a72e
Chris Palmour <chpalmour@gmail>
2017-08-01 20:53:53 -0400

    Q9 second attempt.

--- sts_workspace/src/com/revature/q9/Q9.java
+++ sts_workspace/src/com/revature/q9/Q9.java
@@ -7,6 +7,15 @@ public class Q9 {
 	public static void main(String[] args) {
 		ArrayList<Integer> arr = seive(100);
 		for(Integer i: arr) {System.out.print(i + " ");}
+		System.out.println();
+		//that way was dumb though, and not exactly what the question wanted
+		//here's a way that's not so stupid
+		
+		ArrayList<Integer> secondTry = new ArrayList<>();
+		for(int i=1; i<101; i++) {secondTry.add(i);}
+		for(Integer i: secondTry) {
+			if(isPrime(i)) {System.out.print(i + " ");}
+		}
 	}
 
 	public static ArrayList<Integer> seive(int n){
@@ -30,5 +39,25 @@ public class Q9 {
 			}
 		}
 		return result;
+	}
+	
+	public static boolean isPrime(int n) {
+		
+		if(n<2) {return false;}
+		if(n==2) {return true;}
+		
+		int sqrt = (int)Math.ceil(Math.sqrt(n));
+		/*
+		 * check if input is divisible by any number from 2 upto ceiling of the number's 
+		 * square root. If we pass the square root, and our input has not been divisible by 
+		 * any of the previous numbers, the number must be prime. (bc to be a multiple of a 
+		 * number larger than its square root, our input would also have to be a multiple of a
+		 * number smaller than its square root).
+		 */
+		for(int i=2; i<=sqrt; i++) {
+			if(n%i==0) {return false;}
+		}
+		
+		return true;
 	}
 }
