f2940d6dffa0da2e16b0cc6b72baea45506c744c
Josh Post <joshpost94@gmail.com>
2017-08-07 16:01:50 -0400

    Delete SuperUserDao.java

--- src/main/java/com/revature/superuser/SuperUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.revature.superuser;
-
-public interface SuperUserDao {
-	public void viewUsers();
-	public void removeUser(Integer id);
-	public void updateUser(Integer user, String username, String pass);
-}
