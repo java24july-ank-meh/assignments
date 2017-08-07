1f3133f9a698479d63fcb38d9cdc121754883cbb
Josh Post <joshpost94@gmail.com>
2017-08-07 16:02:25 -0400

    Delete UserDao.java

--- src/main/java/com/revature/users/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature.users;
-
-import com.revature.customers.Customer;
-
-public interface UserDao {
-	public User getUser(String username,String pass);
-	public void addUser(String username, String pass);
-	public Customer checkUser(String username);
-}
