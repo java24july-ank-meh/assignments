9918a7d2d62d8cf313c86cc20aab2890afaa61e3
Devin Thomas <30440053+devinthomas45@users.noreply.github.com>
2017-08-11 11:46:48 -0400

    get connection

--- Project 1/Team DK Project/src/main/java/com/revature/Test/Project1Test.java
+++ Project 1/Team DK Project/src/main/java/com/revature/Test/Project1Test.java
@@ -1,8 +1,24 @@
 package com.revature.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.revature.utilities.ConnectionUtil;
+
 public class Project1Test {
 
 	@Test
-	
+	public final void testConnection() {
+		try {
+			ConnectionUtil.getConection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			System.out.println("Error in connection");
+		}
+	}
 	
 }
