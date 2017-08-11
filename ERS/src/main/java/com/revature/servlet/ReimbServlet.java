5790559639002aa49bd327d0c5f635ff36522d7c
Christian Roach <compcritter@gmail.com>
2017-08-11 15:48:25 -0400

    fully functional reimbservlet

--- ERS/src/main/java/com/revature/servlet/ReimbServlet.java
+++ ERS/src/main/java/com/revature/servlet/ReimbServlet.java
@@ -18,21 +18,27 @@ public class ReimbServlet extends HttpServlet{
 	ERSDAO empdao = new ERSDAOImpl();
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		double amount = Double.parseDouble(req.getParameter("amount"));
-		String description = req.getParameter("description");
-		Date submitted = Date.valueOf(LocalDate.now());
-		int author = ((User) req.getSession().getAttribute("user")).getId();
-		int type = 1;// Integer.parseInt(req.getParameter("type"));
-		int status = 1;
-
-		if (empdao.submitReimb(
-				new Reimbursement(0, amount, description, null, submitted, null, author, 0, type, status))) {
-			// on success
-		} else {
-			// on fail
-		}
-
 		PrintWriter out = resp.getWriter();
-
+		try {
+			double amount = Double.parseDouble(req.getParameter("amount"));
+			String description = req.getParameter("description");
+			Date submitted = Date.valueOf(LocalDate.now());
+			int author = ((User) req.getSession().getAttribute("user")).getId();
+			int type = Integer.parseInt(req.getParameter("type"));
+			int status = 1;
+			
+	
+			if (empdao.submitReimb(
+				new Reimbursement(0, amount, description, null, submitted, null, author, 0, type, status))) {
+				//req.getRequestDispatcher("/employeehome.html").forward(req, resp);
+				out.println("Request submitted. Hooplah!");
+			} else {
+				out.println("ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+			}
+		}catch (NumberFormatException|NullPointerException e) {
+			//output invalid fields stufferonis
+			e.printStackTrace();
+			req.getRequestDispatcher("/employeehome.html").forward(req, resp);
+		} 
 	}
 }
